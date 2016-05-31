a7327eebd03545aa7ed168c7466aba5e0e546b36
stratos-gounidellis <t8130029@dias.aueb.gr>
2016-05-31 10:36:40 +0300

    License Headers Added

--- RegexFEK.php
+++ RegexFEK.php
@@ -1,6 +1,23 @@
 <?php 
 
 require_once('postRequest.php');
+
+/**
+* Copyright 2016, SciFY NPO - http://www.scify.org
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 /**
 * This is the function which checks the content
 * of the WordPress webpage in order to find
