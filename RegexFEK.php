2c83e0a7b0a46d9f8356ad6766c59124c8deabd4
stratos-gounidellis <t8130029@dias.aueb.gr>
2016-06-01 17:44:55 +0300

    Documentation Edit

--- RegexFEK.php
+++ RegexFEK.php
@@ -1,22 +1,31 @@
 <?php 
 
-require_once('postRequest.php');
+/**
+ * Copyright 2016 , SciFY NPO - http://www.scify.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
-* Copyright 2016, SciFY NPO - http://www.scify.org
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * @link       http://thewildpointers.com/
+ * @since      1.0.0
+ * @package    WPLawDetectorGR
+ * @author     WildPointers <2wildpointers@gmail.com>
+ */
+
+
+require_once('postRequest.php');
+
 
 /**
 * This is the function which checks the content
@@ -25,7 +34,7 @@ require_once('postRequest.php');
 * @param content is the content of the
 * Wordpress webpage
 */
-public function regexFEK($content){
+function regexFEK($content){
 	/**
 	* the pattern that should be looked for in order to identify
 	* a reference to the Newspaper of the Government
